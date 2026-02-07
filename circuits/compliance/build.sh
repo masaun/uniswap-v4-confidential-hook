0fab9b86f39426108f74970664a5f52fdc3b9d1e
masaun <masaun@protonmail.com>
2026-02-08 08:53:50 +0900

    chore/circuit (Policy & Strategy): Update the build.sh

--- circuits/compliance/build.sh
+++ circuits/compliance/build.sh
@@ -8,17 +8,17 @@ echo "Circuit version: $VERSION"
 rm -rf target
 
 # Install Noir/Nargo
-#echo "Install the Noir/Nargo v1.0.0-beta.17..."
-#noirup --version 1.0.0-beta.17
+#echo "Install the Noir/Nargo v1.0.0-beta.18..."
+#noirup --version 1.0.0-beta.18
 
-# Align the Noir/Nargo version (v1.0.0-beta.17) and bb.js version (v3.0.0-nightly.20251104) of the local machine.
-#echo "Install the bb.js version v3.0.0-nightly.20251104..."
-#bbup --version 3.0.0-nightly.20251104 (Previous bb.js version: v0.87.0)
+# Align the Noir/Nargo version (v1.0.0-beta.18) and bb.js version (v3.0.0-devnet.6-patch.1) of the local machine.
+#echo "Install the bb.js version v3.0.0-devnet.6-patch.1..."
+#bbup --version 3.0.0-devnet.6-patch.1 (Previous bb.js version: v0.87.0)
 
-echo "Check the Noir/Nargo version of the local machine (This version is supposed to be v1.0.0-beta.17)..."
+echo "Check the Noir/Nargo version of the local machine (This version is supposed to be v1.0.0-beta.18)..."
 nargo -V
 
-echo "Check the bb.js version of the local machine (This version is supposed to be v0.87.0)..."
+echo "Check the bb.js version of the local machine (This version is supposed to be v3.0.0-devnet.6-patch.1)..."
 bb --version
 
 # Compile the ZK circuit
@@ -40,8 +40,8 @@ echo "Copying compliance.json to scripts/circuit-artifacts/compliance/compliance
 cp target/compliance.json "../../scripts/circuit-artifacts/compliance/compliance-$VERSION/compliance.json"
 
 echo "Generating a vkey (verification key)..."
-bb write_vk -b ./target/compliance.json -o ./target/vk --oracle_hash keccak   # bb.js v3.0.0-nightly.20251104
-#bb write_vk -b ./target/compliance.json -o ./target/vk --oracle_hash keccak  # bb.js v0.87.0 (Same with v3.0.0-nightly.20251104)
+bb write_vk -b ./target/compliance.json -o ./target/vk --oracle_hash keccak   # bb.js v3.0.0-devnet.6-patch.1
+#bb write_vk -b ./target/compliance.json -o ./target/vk --oracle_hash keccak  # bb.js v0.87.0 (Same with v3.0.0-devnet.6-patch.1)
 
 echo "Generating vk.json to scripts/circuit-artifacts/compliance/compliance-$VERSION..."
 node -e "const fs = require('fs'); fs.writeFileSync('../../scripts/circuit-artifacts/compliance/compliance-$VERSION/vk.json', JSON.stringify(Array.from(Uint8Array.from(fs.readFileSync('./target/vk/vk')))));"
